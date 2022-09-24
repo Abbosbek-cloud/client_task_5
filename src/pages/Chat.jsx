c9a8c5733aa4a5df5fdbe9ef733313a08619c456
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-09-24 13:36:57 +0500

    Login page completed

--- src/pages/Chat.jsx
+++ src/pages/Chat.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
+import Sidebar from "../components/Sidebar";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return (
+    <Row>
+      <Col md={4}>
+        <Sidebar />
+      </Col>
+      <Col md={8}></Col>
+    </Row>
+  );
 };
 
 export default Chat;
