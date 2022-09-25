324175f0554fcf338908c84512df3f91f3021184
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2022-09-25 19:56:56 +0500

    little bug fixed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { config } from "dotenv";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -7,8 +6,6 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import persistStore from "redux-persist/es/persistStore";
 import store from "./store";
-
-config();
 
 const persistedStore = persistStore(store);
 
